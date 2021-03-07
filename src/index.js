2afad2520be5e3d3a0760ba1e85f1d703068b5a2
VelislavKarastoychev <exel_mmm@abv.bg>
2021-03-07 13:22:46 +0200

    Initial architecture of the Statistics s=class.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 'use strict'
+
+class Statistics {
+    constructor (parameters) {
+        if (controllers.IsParametersCorrect(parameters)) {
+            models.CreateStatisticsInstance(this, parameters) 
+        } else {
+            warnings.IncompleteParametersInStatisticsConstructor()
+            parameters = models.TryToSetByDefault(parameters)
+            return new Statistics(parameters)
+        }
+    }
+    set data (data_array) {
+        if (controllers.IsDataCorrect(data_array)) this.__data__ = data_array
+        else errors.IncorrectDataInDataSetter()
+    }
+    set_data (data_array) {
+        this.data = data_array
+        return this
+    }
+    set type (type) {
+        //
+    }
+}
